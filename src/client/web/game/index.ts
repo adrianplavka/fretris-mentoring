63568f160da7e16ea7ec59d8038c6756eddfdaf1
Adrian Plavka <adrian.plavka@gmail.com>
2019-03-05 19:51:07 +0100

    Merged all pull requests

--- src/client/web/game/index.ts
+++ src/client/web/game/index.ts
@@ -61,11 +61,7 @@ export class Shape {
     
 
         // Your code goes here.
-<<<<<<< HEAD
         return this.move(0, 1);
-=======
-        return this.move(0,1);
->>>>>>> d1ad4410a6d574763cdbf4ecd747d2ee07abf727
     }
 
     // Return a set of points showing where this shape would be if we moved left by one
